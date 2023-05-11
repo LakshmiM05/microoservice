347023b08f2645d3f37ce55b048bede96771f33e
Lakshmi <lakshmigid@gmail.com>
2023-05-11 19:59:29 +0530

    Add usermanagement related existing project files to Git

---
+++ src/main/java/com/example/microservice/usermanagementservice/UserService.java
@@ -0,0 +1,20 @@
+package com.example.microservice.usermanagementservice;
+
+
+
+import java.util.List;
+
+
+public interface UserService {
+	
+	User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+
+}
